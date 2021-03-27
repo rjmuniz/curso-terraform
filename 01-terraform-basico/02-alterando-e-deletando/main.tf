6499e77a5c40523b10f0b61dc692a65a068e6cf7
Ricardo Muniz <rjmuniz@gmail.com>
2021-03-27 04:11:30 +0000

    add aula 1 e 2

---
+++ 01-terraform-basico/02-alterando-e-deletando/main.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+    profile = "default"
+    region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "FirstBucket" {
+    bucket  = "my-tf-test-bucket-rjmuniz"
+    acl     = "private"
+
+    tags = {
+      Name          = "My bucket alterado"
+      Environment   = "Prod"
+    }
+  
+}
